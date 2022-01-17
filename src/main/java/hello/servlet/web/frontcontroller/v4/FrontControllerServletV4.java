0f1686357b593ff322a6e4a0a2a26e371b88e363
ojw <ohjiwooo@gmail.com>
2022-01-17 17:46:02 +0900

    MVC 프레임워크 만들기

--- src/main/java/hello/servlet/web/frontcontroller/v4/FrontControllerServletV4.java
+++ src/main/java/hello/servlet/web/frontcontroller/v4/FrontControllerServletV4.java
@@ -1,10 +1,14 @@
-package hello.servlet.web.frontcontroller.v3;
+package hello.servlet.web.frontcontroller.v4;
 
 import hello.servlet.web.frontcontroller.ModelView;
 import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v3.ControllerV3;
 import hello.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
 import hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
 import hello.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+import hello.servlet.web.frontcontroller.v4.controller.MemberFormControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberListControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberSaveControllerV4;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,15 +19,15 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-@WebServlet(name="frontControllerServlet3", urlPatterns = "/front-controller/v3/*") //v3 하위 모든것에 매칭
-public class FrontControllerServletV3 extends HttpServlet {
+@WebServlet(name="frontControllerServlet4", urlPatterns = "/front-controller/v4/*") //v4 하위 모든것에 매칭
+public class FrontControllerServletV4 extends HttpServlet {
 
-    private Map<String, ControllerV3> controllerMap = new HashMap<>();
+    private Map<String, ControllerV4> controllerMap = new HashMap<>();
 
-    public FrontControllerServletV3() {
-        controllerMap.put("/front-controller/v3/members/new-form",new MemberFormControllerV3());
-        controllerMap.put("/front-controller/v3/members/save",new MemberSaveControllerV3());
-        controllerMap.put("/front-controller/v3/members",new MemberListControllerV3());
+    public FrontControllerServletV4() {
+        controllerMap.put("/front-controller/v4/members/new-form",new MemberFormControllerV4());
+        controllerMap.put("/front-controller/v4/members/save",new MemberSaveControllerV4());
+        controllerMap.put("/front-controller/v4/members",new MemberListControllerV4());
 
     }
 
@@ -31,22 +35,20 @@ public class FrontControllerServletV3 extends HttpServlet {
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         String requestURI = request.getRequestURI();
-        ControllerV3 controller = controllerMap.get(requestURI);
+        ControllerV4 controller = controllerMap.get(requestURI);
         if(controller==null){
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
             return;
         }
         //param을 다 꺼내서 map에 저장
         Map<String, String> paramMap = getParamMap(request);
-
+        Map<String,Object> model = new HashMap<>();
         //가지고 controller 에 가서 비즈니스 로직 실행 + 뷰 이름 생성 후 model(뷰 이름+ 데이터 가진 클래스) 반환
-        ModelView modelView = controller.process(paramMap);
-        //model에서 뷰 이름 얻음
-        String viewName = modelView.getViewName();
+        String viewName = controller.process(paramMap, model);
         //얻은 이름을 url로 고쳐서 뷰 생성
         MyView view = getView(viewName);
         //뷰에서 url을 request에 넣고 dispatch
-        view.render(modelView.getModel(),request, response);
+        view.render(model,request, response);
 
 
     }
